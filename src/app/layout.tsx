3600c1e9a336199105bbbe09c59327d546edfe4b
Giorgio-g97 <giorgio.g97@gmail.com>
2024-11-21 20:10:53 +0100

    moved components folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { Yellowtail } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
+import Navbar from "../components/Navbar";
 
 const yellowtail = Yellowtail({
   subsets: ["latin"],
