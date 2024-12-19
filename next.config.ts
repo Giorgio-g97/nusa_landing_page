d1c8bdef9de01a30c11c666d3dd16f69f683100a
Giorgio-g97 <giorgio.g97@gmail.com>
2024-12-19 21:55:50 +0100

    env problem

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ module.exports = {
   images: {
     domains: ['scontent.fbri4-2.fna.fbcdn.net', "scontent.fbri4-1.fna.fbcdn.net"],
   },
-  env: {
-    RESEND_API_KEY: process.env.RESEND_API_KEY,
-  },
+  // env: {
+  //   NEXT_PUBLIC_RESEND_API_KEY: process.env.NEXT_PUBLIC_RESEND_API_KEY,
+  // },
 }
 
--- src/lib/email.ts
+++ src/lib/email.ts
@@ -5,7 +5,7 @@ import { formSchema } from "./schemas";
 import {Resend} from "resend";
 import { EmailTemplate } from "@/components/email-template";
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY)
 
 export async function POST(emailFormData: z.infer<typeof formSchema>) {
   
